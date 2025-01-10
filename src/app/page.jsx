8869d85421fd064c46b52dd094595e3c393d9cec
harshit-exe <nikamharshit55@gmail.com>
2025-01-11 01:43:53 +0530

    Update page.jsx

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,29 +1,5 @@
-
-
-
-// const Page = () => {
-//   return (
-//     <div className="overflow-x-hidden">
-//       <NavBar />
-//       <div className="ml-0 md:ml-20">
-//         <Home />
-//         <WhyChooseUs />
-//         <Banner {...BannerData} />
-//         <Banner {...BannerData2} reverse={true} />
-//         <FAQ />
-//       </div>
-//       <Footer />
-//     </div>
-//   );
-// };
-
-// export default Page;
-import Footer from "@/components/Footer";
-import FAQ from "@/components/Home/FAQ";
-import Home from "@/components/Home/home";
-import WhyChooseUs from "@/components/Home/WhyChooseUs";
-import NavBar from "@/components/NavBar";
 import React from "react";
+import NavBar from "@/components/NavBar";
 
 const page = () => {
  
@@ -31,12 +7,6 @@ const page = () => {
     <>
       <div className="overflow-x-hidden">
         <NavBar />
-        <div className="ml-0 md:ml-20">
-          <Home />
-          <WhyChooseUs />
-          <FAQ />
-        </div>
-        
       </div>
     </>
   );
