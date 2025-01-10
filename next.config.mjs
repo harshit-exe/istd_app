81b6476e439d2df0fecb7e24d214aee00dac53d7
Aman Gupta <141570525+imamangupta@users.noreply.github.com>
2025-01-11 01:03:51 +0530

    update fix bug

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        domains: ['www.linkedin.com','media.licdn.com'],
+  images: {
+    domains: ['www.linkedin.com', 'media.licdn.com'],
 
-      },
+  },
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.js$/,
+      type: 'javascript/auto', // Allows CommonJS imports
+    });
+    return config;
+  },
 };
 
 export default nextConfig;
