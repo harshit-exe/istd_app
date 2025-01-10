c1e78af9ca24662b96eadf4dfba9f680f7d553ce
prajjvaljaiswal <prajjaiswal158@gmail.com>
2025-01-10 13:24:18 +0530

    -  landing page improvements

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,18 @@
 import Footer from '@/components/Footer'
 import NavBar from '@/components/NavBar'
 import React from 'react'
+import Home from '@/components/Home/home'
+import WhyChooseUs from '@/components/Home/WhyChooseUs'
 
 const page = () => {
   return (
-    <div>
-      <NavBar/>
-      <Footer/>
+    <div className='overflow-x-hidden'>
+      <NavBar />
+      <div className='ml-0 md:ml-20'>
+        <Home />
+        <WhyChooseUs />
+      </div>
+      <Footer />
     </div>
   )
 }
